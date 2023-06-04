3a1f89bb184a2f70f386689495b45cd44d37eb84
Your Name <william.black1983@gmail.com>
2023-06-04 17:44:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget() {
+    return Math.floor(Math.random() * 10);
+  }
+
+  function compareGuesses(userGuess, computerGuess, targetNumber) {
+    const userDifference = Math.abs(userGuess - targetNumber);
+    const computerDifference = Math.abs(computerGuess - targetNumber);
+  
+    if (userDifference <= computerDifference) {
+      return true; // Human wins
+    } else {
+      return false; // Computer wins
+    }
+  }
+
+  function updateScore(winner) {
+    if (winner === 'human') {
+      humanScore++;
+    } else if (winner === 'computer') {
+      computerScore++;
+    }
+  }
+
+  
+  function advanceRound() {
+    currentRoundNumber++;
+  }
+
+  
+  
